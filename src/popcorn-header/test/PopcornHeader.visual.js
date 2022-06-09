506169c93ab56abf33d5baef7ed5397367fdb089
maria-guerrero <mariadol.gc@gmail.com>
2022-06-09 12:05:18 +0200

    feat: add visual test for header

---
+++ src/popcorn-header/test/PopcornHeader.visual.js
@@ -0,0 +1,15 @@
+import { visualDiff } from "@web/test-runner-visual-regression";
+import { PopcornHeader } from "../PopcornHeader";
+
+if(!customElements.get("popcorn-header")) {
+    customElements.define("popcorn-header", PopcornHeader);
+}
+
+describe("PopcornHeader", () => {
+    it("can diff an element", async () => {
+        const element = document.createElement("popcorn-header");
+        document.body.appendChild(element);
+
+        await visualDiff(element, "popcorn-header");
+    })
+})
