68635db7f768cc004a724de5293fe31874fa6199
Maria Guerrero <mariadol.gc@gmail.com>
2022-06-23 11:12:42 +0200

    test: image placeholder test for PopcornListMovies

--- src/popcorn-list-movies/test/PopcornListMovies.test.js
+++ src/popcorn-list-movies/test/PopcornListMovies.test.js
@@ -7,32 +7,36 @@ const scopedFixture = (template) => fixture(template, { scopedElements });
 
 const movies = [
   {
-    "title": "Batman Begins",
-    "id": "tt0372784",
-    "poster": "/.web-test-runner/assets/img/batman.jpg"
-    },
-    {
-    "title": "Batman v Superman: Dawn of Justice",
-    "id": "tt2975590",
-    "poster": "/.web-test-runner/assets/img/uniquePoster.jpg"
-    },
-]
+    title: "Batman Begins",
+    id: "tt0372784",
+    poster: "/.web-test-runner/assets/img/batman.jpg",
+  },
+  {
+    title: "Batman v Superman: Dawn of Justice",
+    id: "tt2975590",
+    poster: "/.web-test-runner/assets/img/uniquePoster.jpg",
+  },
+  {
+    title: "Spiderman",
+    id: "tt0100669",
+    poster: "N/A",
+  },
+];
 
 describe("PopcornListMovies", () => {
-
   it("should be accessible", async () => {
     const element = await scopedFixture(
-      html `<popcorn-list-movies .movies=${movies}></popcorn-list-movies>`
+      html`<popcorn-list-movies .movies=${movies}></popcorn-list-movies>`
     );
     await expect(element).to.be.accessible();
   });
 
   it("should dispatch event on-click-movie when add button is clicked", async () => {
     const element = await scopedFixture(
-      html `<popcorn-list-movies .movies=${movies}></popcorn-list-movies>`
+      html`<popcorn-list-movies .movies=${movies}></popcorn-list-movies>`
     );
 
-    const addMovieButtons = element.shadowRoot.querySelectorAll('.addButton');
+    const addMovieButtons = element.shadowRoot.querySelectorAll(".addButton");
     await waitUntil(() => addMovieButtons[0]);
     setTimeout(() => {
       addMovieButtons[0].click();
@@ -44,11 +48,23 @@ describe("PopcornListMovies", () => {
 
   it("should return nothing when there are no movies in the homepage", async () => {
     const element = await scopedFixture(
-      html `<popcorn-list-movies></popcorn-list-movies>`
+      html`<popcorn-list-movies></popcorn-list-movies>`
     );
 
-    const popcornListMovies = element.shadowRoot.querySelector('[data-testid="popcorn-list-movies"]');
+    const popcornListMovies = element.shadowRoot.querySelector(
+      '[data-testid="popcorn-list-movies"]'
+    );
 
     expect(popcornListMovies).to.be.null;
+  });
+
+  it("should return a poster from assets folder when the image is N/A", async () => {
+    const element = await scopedFixture(
+      html`<popcorn-list-movies .movies=${movies}></popcorn-list-movies>`
+    );
+
+    const movieImage = element.shadowRoot.querySelectorAll('[data-testid="movieImage"]');
+
+    expect(movieImage[2].src).to.be.equal("http://localhost:8000/assets/img/placeholder.jpg");
   });
 });
